9a92fb46a6e491614a55b36572e0406a794d6ccf
Nakul Kumar Vishwakrma <nakul.vishwakarma@quantela.com>
2025-02-28 13:58:53 +0530

    this is for eservicesv1 branch for userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ import { getActiveUsersByLoginId, getUserById, getUsers, getUsersByLoginId } fro
 import { validateLoginId, validateUserId } from "../middelwares/inputValidator.js";
 
 const router=express.Router();
-
+console.log(`this is new changes to commit`);
 router.get("/:id",getUserById);
 router.post("/",validateUserId,getUsers);
 router.post("/getUsersByLoginId", validateLoginId,getUsersByLoginId);
